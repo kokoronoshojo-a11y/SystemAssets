ca8ab0a8dabe4f3278763d5b346f3a8fd58c7aa7
Shojo 3 <kokoronoshojo@gmail.com>
2026-04-10 23:19:45 -0600

    agente.bat

--- agente.bat
+++ agente.bat
@@ -15,7 +15,7 @@ set "ID_NUBE=%MEM_DIR%\id_temp.txt"
 
 
 
-set "PRUEBA=VER17_PruebaSalvavidas_%USERNAME%_%COMPUTERNAME%"
+set "PRUEBA=VER18_PruebaSalvavidas_%USERNAME%_%COMPUTERNAME%"
 call :FUNC_REPORTAR "!PRUEBA!" 
 
 
